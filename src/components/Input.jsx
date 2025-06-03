ffffeba4508331a696ddd98a76f80f0e89ff802a
adity <adityroy672@gmail.com>
2025-06-03 16:26:20 +0530

    corrected

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -8,7 +8,7 @@ const Input = ({
   icon,
 }) => (
   <div className="mb-4 w-full">
-    <label className="block text-sm font-semibold mb-1 text-zinc-800">
+    <label className="block text-sm font-semibold mb-1 text-gray-700 ">
       {label}
     </label>
     <div className="relative">
@@ -18,10 +18,10 @@ const Input = ({
         value={value}
         onChange={onChange}
         placeholder={placeholder}
-        className="w-full px-4 py-2 border border-gray-500 rounded-lg"
+        className="w-full px-4 outline-none text-gray-800 py-2 border-[2px] border-gray-300 rounded-lg"
       />
       {icon && (
-        <div className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer">
+        <div className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer text-gray-400">
           {icon}
         </div>
       )}

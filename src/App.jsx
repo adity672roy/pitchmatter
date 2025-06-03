ca20a689191716654efaca81ab3bcc880fb29bea
adity <adityroy672@gmail.com>
2025-06-03 14:44:45 +0530

    add login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-const App = () => {
-  return <div className="text-6xl text-red-600">App</div>;
-};
+import React from "react";
+import Login  from "./pages/Login";
+
+function App() {
+  return <Login />;
+}
 
 export default App;
